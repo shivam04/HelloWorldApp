1fc2f0357223b31e432f15eaa420a222c68e6875
Shivam Sinha <sinhashivam04@gmail.com>
2026-07-09 22:49:04 +0530

    paddings

--- App.tsx
+++ App.tsx
@@ -16,7 +16,25 @@ function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <Text>Hello World!</Text>
+        <Text style={{
+          //margin: 100,
+          backgroundColor: 'red',
+          marginLeft: 32,
+          //paddingLeft: 10,
+          marginRight: 32,
+          //paddingRight: 10,
+          marginTop: 10,
+          //paddingTop: 10,
+          marginBottom: 10,
+          //paddingBottom: 10,
+          textAlign: 'right',
+          paddingVertical: 100,
+          //paddingHorizontal: 100,
+          padding: 10
+        }}>Hello World!</Text>
+        <Text style={{
+          backgroundColor: 'red'
+        }}>Hello World 2</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
