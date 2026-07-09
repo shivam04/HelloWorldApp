3a73126b6110aa257cf566b4eab243da9e0c2651
Shivam Sinha <sinhashivam04@gmail.com>
2026-07-09 22:56:28 +0530

    position:

--- App.tsx
+++ App.tsx
@@ -15,8 +15,36 @@ function App() {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Hello World!</Text>
+      <SafeAreaView style={{ backgroundColor: 'green' }}>
+        <Text style={{
+          //margin: 100,
+          backgroundColor: 'blue',
+          marginLeft: 32,
+          //paddingLeft: 10,
+          marginRight: 32,
+          //paddingRight: 10,
+          //marginTop: 10,
+          //paddingTop: 10,
+          //marginBottom: 10,
+          marginVertical: 100,
+          //paddingBottom: 10,
+          textAlign: 'right',
+          paddingVertical: 100,
+          //paddingHorizontal: 100,
+          padding: 10,
+          position: 'relative',
+          zIndex: 2
+        }}>Hello World!</Text>
+
+        <Text style={{
+          backgroundColor: 'red',
+          position: 'absolute',
+          left: 150,
+          right: 100,
+          top: 150,
+          bottom: 100,
+          zIndex: 1
+        }}>Hello World 2</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
